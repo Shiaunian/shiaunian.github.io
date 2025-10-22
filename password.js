34500422bbab6bf27aca547db3d41913b289089f
Shiaunian <loveredmiya@gmail.com>
2025-10-22 08:15:25 +0800

    Update password.js

--- password.js
+++ password.js
@@ -82,5 +82,6 @@ window.ACCOUNTS = [
   {"username":"2702","password":"0905","_comment":"黃暐傑","welcome":"歡迎黃暐傑"},
   {"username":"6808","password":"0320","_comment":"莊庭瑄","welcome":"歡迎莊庭瑄"},
   {"username":"0528","password":"0326","_comment":"劉文華","welcome":"歡迎劉文華"},
-  {"username":"3595","password":"0710","_comment":"陳翠蓉","welcome":"歡迎陳翠蓉"}
+  {"username":"3595","password":"0710","_comment":"陳翠蓉","welcome":"歡迎陳翠蓉"},
+  {"username":"1351","password":"0326","_comment":"張麗曲","welcome":"歡迎張麗曲"}
 ];
