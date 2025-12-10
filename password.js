270ebde314e55a9c877944df4f7ce0a10e4022fe
judy303333 <judy303333@gmail.com>
2025-12-10 10:36:05 +0800

    Update password.js

--- password.js
+++ password.js
@@ -83,8 +83,10 @@ window.ACCOUNTS = [
   {"username":"8691","password":"1216","_comment":"周芝瑩","welcome":"歡迎周芝瑩"},
   {"username":"1720","password":"0114","_comment":"涂瓊文","welcome":"歡迎涂瓊文"},
   {"username":"4580","password":"0206","_comment":"向彥姿","welcome":"歡迎向彥姿"},
+  {"username":"8881","password":"0608","_comment":"郭裔祥","welcome":"歡迎郭裔祥"},
   {"username":"6624","password":"0102","_comment":"李秀貞","welcome":"歡迎李秀貞"}
 ];
+
 
 
 
