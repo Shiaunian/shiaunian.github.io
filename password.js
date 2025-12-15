f76767641ab70fbc03e84ad10c8f6ae312813103
Shiaunian <loveredmiya@gmail.com>
2025-12-15 09:04:25 +0800

    Update fmt.Println message from 'Hello' to 'Goodbye'

--- password.js
+++ password.js
@@ -85,8 +85,10 @@ window.ACCOUNTS = [
   {"username":"4580","password":"0206","_comment":"向彥姿","welcome":"歡迎向彥姿"},
   {"username":"8881","password":"0608","_comment":"郭裔祥","welcome":"歡迎郭裔祥"},
   {"username":"4452","password":"0913","_comment":"楊子房","welcome":"歡迎楊子房"},
-  {"username":"6624","password":"0102","_comment":"李秀貞","welcome":"歡迎李秀貞"}
+  {"username":"6624","password":"0102","_comment":"李秀貞","welcome":"歡迎李秀貞"},
+  {"username":"1846","password":"1108","_comment":"林亭君","welcome":"歡迎林亭君"}
 ];
+
 
 
 
